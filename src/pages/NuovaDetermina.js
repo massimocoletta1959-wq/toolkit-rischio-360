e95cb0f3aabc40f32fe8bf79849750d81c1ff58c
massimocoletta1959-wq <massimocoletta1959@gmail.com>
2026-08-30 20:05:41 +0200

    Fix build: rimosso commento eslint non configurato

--- src/pages/NuovaDetermina.js
+++ src/pages/NuovaDetermina.js
@@ -334,7 +334,6 @@ export default function NuovaDetermina() {
     if (step === 6 && !determinaId && !bozzaProvvId && !soloLettura && oggetto.trim()) {
       assicuraBozzaProvvisoria()
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [step])
 
   // Uscita dal wizard: se c'è una bozza provvisoria non confermata, la elimino
